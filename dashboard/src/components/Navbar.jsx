c532fd04876b5c2b7d9b6fa7f79f41693ca80841
ryan <rhynogolfer42@gmail.com>
2024-03-11 16:05:43 -0700

    Adds myfile.md

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 )
 
 const Navbar = () => {
-  const { currentColor, activeMenu, setActiveMenu, handleClick } = useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
@@ -80,6 +80,10 @@ const Navbar = () => {
               className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   )
--- dashboard/src/contexts/ContextProvider.js
+++ dashboard/src/contexts/ContextProvider.js
@@ -11,13 +11,20 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState)
 
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: true });
+    }
 
     return (
         <StateContext.Provider
             value={{
                 activeMenu,
                 setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick
             }}
         >
             {children}
