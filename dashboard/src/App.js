e1415a977bbf6d5e1b897068cd1789d2ebe7e093
ryan <rhynogolfer42@gmail.com>
2024-03-11 21:29:54 -0700

    Adds myfile.md

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -44,7 +44,6 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
-          </div>
 
           <div>
           <Routes>
@@ -75,7 +74,7 @@ const App = () => {
 
               </Routes>
           </div>
-
+          </div>
         </div>
       </BrowserRouter>
     </div>
