df6af15abd82913e82ee569bf8d2ff4724e92ef9
ryan <rhynogolfer42@gmail.com>
2024-03-13 15:57:17 -0700

    Dummy data chart completed

--- dashboard/src/components/Charts/SparkLine.jsx
+++ dashboard/src/components/Charts/SparkLine.jsx
@@ -3,8 +3,27 @@ import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-re
 
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
   return (
-    <SparklineComponent>
-      
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+        tooltipSettings={{
+          visible: true,
+          format: '${x} : data 4{y}',
+          trackLineSettings: {
+            visible: true,
+          }
+        }}
+    >
+      <Inject services={[SparklineTooltip]} />
     </SparklineComponent>
   )
 }
