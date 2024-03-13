32e22a7525052ff675cc72857d2366c4b3800c49
ryan <rhynogolfer42@gmail.com>
2024-03-12 21:50:21 -0700

    Adds myfile.md

--- dashboard/src/components/Charts/SparkLine.jsx
+++ dashboard/src/components/Charts/SparkLine.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
 const SparkLine = () => {
   return (
