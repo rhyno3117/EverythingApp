98a46d2d7a6993501676eb574b70d0b0ee978b47
ryan <rhynogolfer42@gmail.com>
2024-03-18 13:44:08 -0700

    Adds myfile.md

--- dashboard/src/components/Charts/Stacked.jsx
+++ dashboard/src/components/Charts/Stacked.jsx
@@ -3,18 +3,20 @@ import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Leg
 
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
 
-const Stacked = ({ width, height}) => {
+const Stacked = ({ width, height }) => {
   return (
-    <ChartComponent 
-    width={width} 
-    height={height}
-    id='stack chart'
-    // primaryXAxis={stackedPrimaryXAxis}
-    // primaryYAxis={stackedPrimaryYAxis}
-    chartArea={{ border: {width: 0} }}
-    tooltip={{ enable: true }}
+    <ChartComponent
+      width={width}
+      height={height}
+      id='stack chart'
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: 'white' }}
+
     >
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]}/>
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
       <SeriesCollectionDirective>
         {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
       </SeriesCollectionDirective>
