7fe636c08b5e4fee1d57a677f1006281824191bc
ryan <rhynogolfer42@gmail.com>
2024-03-18 20:14:24 -0700

    Adds myfile.md

--- dashboard/src/components/Header.jsx
+++ dashboard/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ category, title }) => (
   <div className=" mb-10">
-    <p className="text-lg text-gray-400">{category}</p>
+    <p className="text-gray-400">{category}</p>
     <p className="text-3xl font-extrabold tracking-tight text-slate-900">
       {title}
     </p>
