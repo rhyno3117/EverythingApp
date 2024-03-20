add56a3954f036d1945b602da5c26747b41401c0
ryan <rhynogolfer42@gmail.com>
2024-03-19 22:27:04 -0700

    Adds myfile.md

--- dashboard/src/pages/Employees.jsx
+++ dashboard/src/pages/Employees.jsx
@@ -6,10 +6,6 @@ import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Employees = () => {
-  const toolbarOptions = ['Search'];
-
-  const editing = { allowDeleting: true, allowEditing: true };
-
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Employees" />
