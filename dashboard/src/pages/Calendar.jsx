67bd489010903e015ddc56be25dd39c47cbcaebb
ryan <rhynogolfer42@gmail.com>
2024-03-22 14:42:04 -0700

    Calender imports

--- dashboard/src/pages/Calendar.jsx
+++ dashboard/src/pages/Calendar.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
 
 const Calender = () => {
   return (
