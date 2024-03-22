8856d45a65981e9d9808e9b9662a046e22b6a2e1
ryan <rhynogolfer42@gmail.com>
2024-03-22 14:37:36 -0700

    Customers done

--- dashboard/src/pages/Customers.jsx
+++ dashboard/src/pages/Customers.jsx
@@ -5,22 +5,16 @@ import { customersData, customersGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Customers = () => {
-  const selectionsettings = { persistSelection: true };
-  const toolbarOptions = ['Delete'];
-  const editing = { allowDeleting: true, allowEditing: true };
-
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Customers" />
       <GridComponent
         dataSource={customersData}
-        enableHover={false}
         allowPaging
-        pageSettings={{ pageCount: 5 }}
-        selectionSettings={selectionsettings}
-        toolbar={toolbarOptions}
-        editSettings={editing}
         allowSorting
+        toolbar={['Delete']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width="auto"
       >
         <ColumnsDirective>
           {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
