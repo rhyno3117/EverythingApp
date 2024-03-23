712e1b260d0095f65afaa10ef9ac3fcac7151467
ryan <rhynogolfer42@gmail.com>
2024-03-23 14:32:49 -0700

    Adds myfile.md

--- dashboard/src/pages/Calendar.jsx
+++ dashboard/src/pages/Calendar.jsx
@@ -9,7 +9,11 @@ const Calender = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category='App' title='Calender' />
-      <ScheduleComponent>
+      <ScheduleComponent
+      height="650px"
+      selectedDate={new Date(2021, 0, 10)}
+      eventSettings={{ dataSource: scheduleData }}
+      >
         <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}/>
       </ScheduleComponent>
     </div>
