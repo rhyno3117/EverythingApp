42babe41f79f443f23c07bd4b9459bb0056e3bd7
ryan <rhynogolfer42@gmail.com>
2024-03-23 19:42:57 -0700

    ColorPicker imports

--- dashboard/src/pages/ColorPicker.jsx
+++ dashboard/src/pages/ColorPicker.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+
+import { Header } from '../components';
 
 const ColorPicker = () => {
   return (
