9e86fff2ddaf09201f0d31b202229ac6bfb5b009
ryan <rhynogolfer42@gmail.com>
2024-03-23 19:24:03 -0700

    Adds myfile.md

--- dashboard/src/pages/Editor.jsx
+++ dashboard/src/pages/Editor.jsx
@@ -6,7 +6,9 @@ import { EditorData } from '../data/dummy';
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+    </div>
   )
 }
 
