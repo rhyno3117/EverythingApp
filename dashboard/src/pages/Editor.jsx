25a90519dfece1e9d24bc2e703f7160951e9d376
ryan <rhynogolfer42@gmail.com>
2024-03-23 19:23:00 -0700

    Editor imports

--- dashboard/src/pages/Editor.jsx
+++ dashboard/src/pages/Editor.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+
+import { Header } from '../components';
+import { EditorData } from '../data/dummy';
 
 const Editor = () => {
   return (
