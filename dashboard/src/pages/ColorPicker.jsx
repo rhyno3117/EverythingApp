218261460e0cbc80c2f4ab2af96450fa7556e05d
ryan <rhynogolfer42@gmail.com>
2024-03-24 20:52:15 -0700

    ColorPicker

--- dashboard/src/pages/ColorPicker.jsx
+++ dashboard/src/pages/ColorPicker.jsx
@@ -6,8 +6,21 @@ import { Header } from '../components';
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="App" title="Color Picker" />
+    <Header category="App" title="Color Picker" />
+    <div className="text-center">
+      <div id="preview" />
+      <div className="flex justify-center items-center gap-20 flex-wrap">
+        <div>
+          <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+          <ColorPickerComponent id="inline-palette" mode="Palette" modeSwitcher={false} inline showButtons={false}/>
+        </div>
+        <div>
+          <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+          <ColorPickerComponent id="inline-picker" mode="Picker" modeSwitcher={false} inline showButtons={false}/>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
