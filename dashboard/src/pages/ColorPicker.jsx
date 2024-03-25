efef0b5efc41d2457c7051c96c3bf6f25969abb3
ryan <rhynogolfer42@gmail.com>
2024-03-24 20:54:50 -0700

    Colorpicker working

--- dashboard/src/pages/ColorPicker.jsx
+++ dashboard/src/pages/ColorPicker.jsx
@@ -3,6 +3,10 @@ import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
 
 import { Header } from '../components';
 
+const change = (args) => {
+  document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+};
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -12,11 +16,25 @@ const ColorPicker = () => {
       <div className="flex justify-center items-center gap-20 flex-wrap">
         <div>
           <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
-          <ColorPickerComponent id="inline-palette" mode="Palette" modeSwitcher={false} inline showButtons={false}/>
+          <ColorPickerComponent 
+          id="inline-palette" 
+          mode="Palette" 
+          modeSwitcher={false} 
+          inline 
+          showButtons={false}
+          change={change}
+          />
         </div>
         <div>
           <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
-          <ColorPickerComponent id="inline-picker" mode="Picker" modeSwitcher={false} inline showButtons={false}/>
+          <ColorPickerComponent 
+          id="inline-picker" 
+          mode="Picker" 
+          modeSwitcher={false} 
+          inline 
+          showButtons={false}
+          change={change}
+          />
         </div>
       </div>
     </div>
