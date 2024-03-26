6a05b7f687a84f9044cea882f03055122b91ac60
ryan <rhynogolfer42@gmail.com>
2024-03-26 14:08:45 -0700

    Adds myfile.md

--- dashboard/src/components/ThemeSettings.jsx
+++ dashboard/src/components/ThemeSettings.jsx
@@ -9,7 +9,9 @@ import { useStateContext } from '../contexts/ContextProvider';
 const ThemeSettings = () => {
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400">
 
+      </div>
     </div>
   )
 }
