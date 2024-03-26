f67ee5413780271942c6e26e46f57f31dc2bd321
ryan <rhynogolfer42@gmail.com>
2024-03-26 14:20:39 -0700

    Adds myfile.md

--- dashboard/src/components/ThemeSettings.jsx
+++ dashboard/src/components/ThemeSettings.jsx
@@ -10,7 +10,17 @@ const ThemeSettings = () => {
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
       <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400">
+        <div className="flex justify-between items-center p-4 ml-4">
+          <p className="font-semibold text-lg"> Settings </p>
+          <button
+           type="button"
+           onClick={() =>{}}
+           style={{ color: 'rgb(153, 171, 180)', borderRadius: '50%' }}
+           className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
+          >
 
+          </button>
+        </div>
       </div>
     </div>
   )
