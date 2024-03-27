bc2bffbf7f6c3759a00d80ffa8881d3dc338d046
ryan <rhynogolfer42@gmail.com>
2024-03-27 01:07:39 -0700

    Them color buttons

--- dashboard/src/components/ThemeSettings.jsx
+++ dashboard/src/components/ThemeSettings.jsx
@@ -51,13 +51,34 @@ const ThemeSettings = () => {
             />
             <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
               Dark
-            </label>
+              </label>
           </div>
-
+        </div>
+        <div className="p-4 border-t-1 border-color ml-4">
+          <p className="font-semibold text-xl ">Theme Colors</p>
+          <div className="flex gap-3">
+            {themeColors.map((item, index) => (
+              <TooltipComponent key={index} content={item.name} position="TopCenter">
+                <div
+                  className="relative mt-2 cursor-pointer flex gap-5 items-center"
+                  key={item.name}
+                >
+                  <button
+                    type="button"
+                    className="h-10 w-10 rounded-full cursor-pointer"
+                    style={{ backgroundColor: item.color }}
+                    onClick={() => {}}
+                  >
+                    <BsCheck className={`ml-2 text-2xl text-white ${false ? 'block' : 'hidden'}`} />
+                  </button>
+                </div>
+              </TooltipComponent>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ThemeSettings;
