6ef87cb82d37ed730674789d9aa9a61584f02a9b
ryan <rhynogolfer42@gmail.com>
2024-04-01 12:32:23 -0700

    Line chart appearing

--- dashboard/src/components/Charts/LineChart.jsx
+++ dashboard/src/components/Charts/LineChart.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip, Chart } from '@syncfusion/ej2-react-charts';
 
 import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
 
 const LineChart = () => {
   return (
-    <div>LineChart</div>
-  )
+<ChartComponent>
+  
+</ChartComponent>
+    )
 }
 
 export default LineChart
