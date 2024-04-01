bd231a94d71168f20bf1ba42571224cb8aa03d6c
ryan <rhynogolfer42@gmail.com>
2024-04-01 12:29:21 -0700

    Line

--- dashboard/src/pages/Charts/Line.jsx
+++ dashboard/src/pages/Charts/Line.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-import { ChartsHeader, LineChart } from '../../components';
+import { Header, LineChart } from '../../components';
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category='chart' title="Inflation Rate" />
+      <div className='w-full'>
+        <LineChart />
+      </div>
+    </div>
   )
 }
 
