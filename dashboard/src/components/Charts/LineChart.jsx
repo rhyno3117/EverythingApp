a2da80b06db20d0e73edc2db1077c6ef45e7c39f
ryan <rhynogolfer42@gmail.com>
2024-04-01 12:42:14 -0700

    Adds myfile.md

--- dashboard/src/components/Charts/LineChart.jsx
+++ dashboard/src/components/Charts/LineChart.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip, Chart } from '@syncfusion/ej2-react-charts';
 
 import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
 
 const LineChart = () => {
   return (
