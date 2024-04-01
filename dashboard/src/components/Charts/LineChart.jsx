0c3b78b818fd04bc11fbee4c77d1d9373b1dba79
ryan <rhynogolfer42@gmail.com>
2024-04-01 12:31:47 -0700

    Imports for linechart

--- dashboard/src/components/Charts/LineChart.jsx
+++ dashboard/src/components/Charts/LineChart.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
 
 const LineChart = () => {
   return (
